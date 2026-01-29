15a09716a8f0b01079f868175ac4da5cb89bfc2f
gyenoch <www.gyenoch@gmail.com>
2026-01-29 02:31:39 +0000

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
-  extends: ["next/core-web-vitals"],
+  extends: ["next/core-web-vitals", "next/typescript"],
   rules: {
+    // Disable unused vars error for underscore-prefixed args
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/no-explicit-any": "warn",
   },
